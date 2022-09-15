d5490411e3fdda358ff8787e759f5ac302953b21
AdityaKshettri <adikshettri1623@gmail.com>
2022-09-15 21:36:41 +0530

    added music player logic

--- app/src/main/java/com/aditya/project/MainActivity.java
+++ app/src/main/java/com/aditya/project/MainActivity.java
@@ -1,8 +1,12 @@
 package com.aditya.project;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -34,13 +38,24 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
                         Toast.makeText(MainActivity.this, "Runtime Permission given!", Toast.LENGTH_SHORT).show();
-                        List<File> songs = fetchSongs(Environment.getExternalStorageDirectory());
+                        ArrayList<File> songs = fetchSongs(Environment.getExternalStorageDirectory());
                         String[] items = new String[songs.size()];
                         for (int i = 0; i < songs.size(); i++) {
                             items[i] = songs.get(i).getName().replace(".mp3", "");
                         }
                         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
                         listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                Intent intent = new Intent(MainActivity.this, PlaySong.class);
+                                String currSong = listView.getItemAtPosition(i).toString();
+                                intent.putExtra("songs", songs);
+                                intent.putExtra("currSong", currSong);
+                                intent.putExtra("position", i);
+                                startActivity(intent);
+                            }
+                        });
                     }
 
                     @Override
@@ -56,15 +71,15 @@ public class MainActivity extends AppCompatActivity {
                 .check();
     }
 
-    public List<File> fetchSongs(File file) {
-        List<File> list = new ArrayList<>();
+    public ArrayList<File> fetchSongs(File file) {
+        ArrayList<File> list = new ArrayList<>();
         File[] songs = file.listFiles();
         if (songs != null) {
             for (File song : songs) {
                 if (!song.isHidden() && song.isDirectory()) {
                     list.addAll(fetchSongs(song));
                 } else {
-                    if (!song.getName().endsWith(".mp3") && !song.getName().startsWith(".")) {
+                    if (song.getName().endsWith(".mp3") && !song.getName().startsWith(".")) {
                         list.add(song);
                     }
                 }
